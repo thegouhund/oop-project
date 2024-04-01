db656313e82e313ca2725cffb268746fced99e3a
thegouhund <alexandresapu1@gmail.com>
2024-04-01 17:17:41 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.tubes {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.fasterxml.jackson.databind;
+
+
+    opens com.tubes to javafx.fxml;
+    exports com.tubes;
+}
