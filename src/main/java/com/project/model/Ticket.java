6bdeb05228b384f3a82ff302f9238588a8eecde6
thegouhund <alexandresapu1@gmail.com>
2024-04-11 00:55:21 +0800

    added Ticket model

--- src/main/java/com/project/model/Ticket.java
+++ src/main/java/com/project/model/Ticket.java
@@ -6,4 +6,25 @@ import java.util.Date;
 public class Ticket {
     private ArrayList<Passenger> passengers;
     private Schedule schedule;
+
+    public Ticket(ArrayList<Passenger> passengers, Schedule schedule) {
+        this.passengers = passengers;
+        this.schedule = schedule;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
 }
