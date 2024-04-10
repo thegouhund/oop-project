bf9be8f72f887117f89d46f27b2f205df877dd52
thegouhund <alexandresapu1@gmail.com>
2024-04-11 00:53:42 +0800

    better naming

--- src/main/java/com/project/utils/RandomUtils.java
+++ src/main/java/com/project/utils/RandomUtils.java
@@ -2,7 +2,7 @@ package com.project.utils;
 
 import java.util.random.RandomGenerator;
 
-public class Random {
+public class RandomUtils {
     public static RandomGenerator random() {
         return RandomGenerator.of("L128X256MixRandom");
     }
