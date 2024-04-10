29f15c9e7087a87c36b0fa12f81264911baaa9bf
thegouhund <alexandresapu1@gmail.com>
2024-04-11 00:53:26 +0800

    added mysql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,13 @@ module com.tubes {
     requires org.kordamp.bootstrapfx.core;
     requires com.fasterxml.jackson.databind;
     requires MaterialFX;
+    requires java.sql;
 //    requires io.github.palexdev.materialfx;
 
     opens com.project to javafx.fxml;
     exports com.project;
     exports com.project.controller;
     opens com.project.controller to javafx.fxml;
+    exports com.project.utils;
+    opens com.project.utils to javafx.fxml;
 }
